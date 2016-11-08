6b369a6294f831b1ca3f58a221d94cb713497146
Matteo Gavagnin <m@macteo.it>
2016-11-08 09:10:48 +0100

    Created framework target

---
+++ CHCSVParser/CHCSVParser.h
@@ -0,0 +1,19 @@
+//
+//  CHCSVParser.h
+//  CHCSVParser
+//
+//  Created by Matteo Gavagnin on 08/11/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CHCSVParser.
+FOUNDATION_EXPORT double CHCSVParserVersionNumber;
+
+//! Project version string for CHCSVParser.
+FOUNDATION_EXPORT const unsigned char CHCSVParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHCSVParser/PublicHeader.h>
+
+
